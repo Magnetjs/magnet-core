6f8dc7680bd1e4efee4bcc546499418e3e0677f7
Kieve Chua <kievechua@gmail.com>
2017-02-03 17:14:54 +0800

    Disable start function, support teardown in convert and refactor index.js

--- src/base.js
+++ src/base.js
@@ -1,8 +1,8 @@
 export default class Base {
-  constructor(app = {}, options = {}) {
-    this.app = app;
-    this.log = app.log;
-    this.config = app.config;
-    this.options = options;
+  constructor (app = {}, options = {}) {
+    this.app = app
+    this.log = app.log
+    this.config = app.config
+    this.options = options
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,97 +1,97 @@
+import flatten from 'lodash/flatten'
+
+function consoleInfo (app, message) {
+  if (app && app.log && app.log.info) {
+    app.log.info(message)
+  } else {
+    console.log(message)
+  }
+}
+
+function consoleError (app, message) {
+  if (app && app.log && app.log.error) {
+    app.log.error(message)
+  } else {
+    console.error(app, message)
+  }
+}
+
+async function performTasks (app, modules) {
+  const tds = []
+  try {
+    for (const Module of modules) {
+      if (Array.isArray(Module)) {
+        tds.push(await performTasks(app, Module))
+      } else {
+        let task
+        if (Module.module) {
+          task = new Module.module(app, Module.options)
+        } else {
+          task = new Module(app)
+        }
+
+        if (task.setup) {
+          await task.setup()
+        }
+
+        if (task.teardown) {
+          tds.push(task.teardown.bind(task))
+        }
+      }
+    }
+
+    return tds
+  } catch (err) {
+    consoleError(app, err)
+    return tds
+  }
+}
+
 /**
  * We can change setup to be middleware based, but now let keep it simple with array only.
  */
-export default async function Magnet(modules = []) {
-  let app = {};
-  let starts = [];
-  let teardowns = [];
-
-  function consoleInfo(app, message) {
-    if (app && app.log && app.log.info) {
-      app.log.info(message);
-    } else {
-      console.log(message);
-    }
-  }
-
-  function consoleError(app, message) {
-    if (app && app.log && app.log.error) {
-      app.log.error(message);
-    } else {
-      console.error(app, message);
-    }
-  }
-
-  async function performTasks(arr) {
-    let Modules = arr.shift();
-
-    if (!Array.isArray(Modules)) {
-      Modules = [Modules];
-    }
-
-    let setups = [];
-    for (let Module of Modules) {
-      let task;
-      if (Module.module) {
-        task = new Module.module(app, Module.options);
-      } else {
-        task = new Module(app);
-      }
-
-      if (task.setup) {
-        setups.push(await task.setup());
-      }
-
-      if (task.start) {
-        starts.push(task.start.bind(task));
-      }
-
-      if (task.teardown) {
-        teardowns.push(task.teardown.bind(task));
-      }
-    }
-
-    await setups;
-
-    if (arr.length) {
-      await performTasks(arr);
-    }
-  }
-
-  async function errorHandler(err) {
-    try {
-      let closing = [];
-      for (let teardown of teardowns) {
-        closing.push(teardown());
-      }
-      await Promise.all(closing);
-    } catch (err) {
-      consoleError(app, err);
-    } finally {
-      consoleInfo(app, 'Finally shutdown server');
-      process.kill(process.pid, 'SIGUSR2');
-    }
-  }
-
-  process.once('uncaughtException', errorHandler);
-  process.once('SIGUSR2', errorHandler);
-  process.once('SIGINT', errorHandler);
-
+export default async function Magnet (modules = []) {
+  let app = {}
   try {
-    if (modules.length) {
-      await performTasks(modules);
+    let teardowns = []
 
-      for (let start of starts.reverse()) {
-        await start();
+    const errorHandler = async function (err) {
+      if (err) {
+        consoleError(app, err)
       }
 
-      consoleInfo(app,'Ready');
-    } else {
-      consoleInfo(app,'No modules provided.');
+      try {
+        await Promise.all(
+          flatten(teardowns)
+            .reverse()
+            .map((teardown) => teardown())
+        )
+      } catch (err) {
+        consoleError(app, err)
+      } finally {
+        consoleInfo(app, 'Complete teardown all Magnet\'s module')
+        process.kill(process.pid, 'SIGUSR2')
+      }
     }
 
-    return app;
+    process.once('uncaughtException', errorHandler)
+    process.once('SIGUSR2', errorHandler)
+    process.once('SIGINT', errorHandler)
+
+    if (!Array.isArray(modules)) {
+      consoleError(app, 'Modules should pass in as array')
+      return
+    } else if (!modules.length) {
+      consoleInfo(app, 'No modules provided')
+      return
+    }
+
+    teardowns = await performTasks(app, modules)
+
+    consoleInfo(app, 'Ready')
+
+    return app
   } catch (err) {
-    consoleError(app, err.stack);
+    consoleError(app, err.stack)
   }
 }
