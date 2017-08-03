ecf36b0210e42c59b41a39e09a97433df0e6ac90
Kieve Chua <kievechua@gmail.com>
2017-08-03 10:16:29 +0800

    update fs-extra

--- src/loader.ts
+++ src/loader.ts
@@ -5,6 +5,7 @@ export function fromNode (modulePath: string, options?: any) {
   try {
     mod = require(modulePath).default
   } catch (err) {
+    console.log('sssss', err)
     if (err.code === 'MODULE_NOT_FOUND') {
       // TODO: proper way to display error
       // Some error return as MODULE_NOT_FOUND
